f2ab93b99cd601ada5fd400e73800e87524af7ea
Paweł Staniul <pstaniul@gmail.com>
2025-07-06 20:14:59 +0200

    Created ProjectFormComponent and ProjectImageComponent

---
+++ frontend/src/app/admin/components/project-image-upload/project-image-upload.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-image-upload',
+  imports: [],
+  templateUrl: './project-image-upload.component.html',
+  styleUrl: './project-image-upload.component.scss'
+})
+export class ProjectImageUploadComponent {
+
+}
